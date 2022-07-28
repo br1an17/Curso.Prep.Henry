f0c8682fff03dc174f399b9c491ff0033ef30ccb
br1an17 <brianzairpaez@gmail.com>
2022-07-28 16:23:25 -0300

    tarea 4 terminada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -93,14 +93,18 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
-  if( usuario['password'] === password) ;{return true;
-  } return false
+  if ( usuario ["password"] !== password) {
+    return false;
+  } else {return true}
 }
 
 function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+   usuario.password = nuevaPassword;
+    return usuario
+
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -108,6 +112,9 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+   usuario.amigos.push(nuevoAmigo);
+   return usuario;
+
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -116,6 +123,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+ 
+  for(var i = 0 ; i < usuarios.length ; i ++ ) {
+    usuarios[i].esPremium = true ;
+  } return usuarios
 }
 
 function sumarLikesDeUsuario(usuario) {
@@ -125,6 +136,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma = 0
+   for (var i = 0 ; i < usuario.posts.length ; i++){
+    suma = suma + usuario.posts[i].likes}
+
+  return suma
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -137,6 +153,12 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+
+  producto.calcularPrecioDescuento = function(){
+    return this.precio - (this.precio * this.porcentajeDeDescuento) ;
+  } 
+
+  return producto;
 
 }
 
